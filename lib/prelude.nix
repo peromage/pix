b010a50fd1870e1800c3e9ac4ef7436b76d5914d
Fang Deng <fang@elfang.com>
2025-01-09 12:49:13 -0500

    pix: Update lib.listDir to pass full path to predicate

--- lib/prelude.nix
+++ lib/prelude.nix
@@ -33,9 +33,10 @@ in with self; {
      Type:
        listDir :: (String -> String -> Bool) -> Path -> [String]
   */
-  listDir = pred: dir: with builtins; lib.mapAttrsToList
-    (n: t: toString (dir + "/${n}"))
-    (lib.filterAttrs pred (readDir dir));
+  listDir = pred: dir: with lib; builtins.attrNames
+    (filterAttrs pred (mapAttrs'
+      (name: type: nameValuePair (builtins.toString (dir + "/${name}")) type)
+      (builtins.readDir dir)));
 
   /* Predications used for `listDir'. */
   notPred = pred: name: type: ! pred name type;
@@ -45,11 +46,10 @@ in with self; {
   isDirectoryType = name: type: type == "directory";
   isRegularType = name: type: type == "regular";
   isSymbolicType = name: type: type == "symlink";
-  isDefaultNix = name: type: name == "default.nix";
-  isNixFile = name: type: isNotDirType name type && builtins.match ".+\\.nix$" name != null;
+  isDefaultNix = name: type: (builtins.baseNameOf name) == "default.nix";
+  isNixFile = andPred isRegularType (name: type: builtins.match ".+\\.nix$" name != null);
   isDisabled = name: type: builtins.match "^DISABLED_.*" name != null;
-  isImportable = name: type: isDirectoryType name type || isNixFile name type;
-  hasDefaultNix = name: type: with builtins; isDirectoryType name type && hasAttr "default.nix"  (readDir name);
+  hasDefaultNix = andPred isDirectoryType (name: type: with builtins; hasAttr "default.nix"  (readDir name));
 
   /* Return the basename without .nix extension
 
