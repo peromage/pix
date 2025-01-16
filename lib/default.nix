6806bf9fa87583f5595e017e927d030c0514428e
Fang Deng <fang@elfang.com>
2025-01-15 20:36:54 -0500

    pix: Simplify lib imports

--- lib/default.nix
+++ lib/default.nix
@@ -1,15 +1,11 @@
-### Top level of the common code
-
 { nixpkgs, ... }@args:
 
 let
   lib = nixpkgs.lib;
-  prelude = let x = (import ./prelude.nix) (args // { self = x; }); in x;
-
-  ## Import all nix files within this folder
-  self = with lib; with prelude; foldl'
-    (acc: x: acc // x)
-    {}
-    (attrValues (mapImport (call (args // { inherit self; })) ./.));
+  self = with lib; foldl' (acc: x: acc // (import x (args // { inherit self; }))) {} [
+    ./modules.nix
+    ./prelude.nix
+    ./trivial.nix
+  ];
 
 in self
