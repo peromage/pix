fc683699f25a72f5668654367106cffddc09f62b
Fang Deng <fang@elfang.com>
2025-01-16 21:44:21 -0500

    pix: Use explicit lib args

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,8 @@
-{ nixpkgs, ... }@args:
+{ nixpkgs }:
 
 let
   lib = nixpkgs.lib;
-  self = with lib; foldl' (acc: x: acc // (import x (args // { inherit self; }))) {} [
+  self = with lib; foldl' (acc: x: acc // (import x { inherit nixpkgs self; })) {} [
     ./modules.nix
     ./prelude.nix
     ./trivial.nix
