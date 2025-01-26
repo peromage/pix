134af78afbd51a3427306550ff84b0cea95ce450
Fang Deng <fang@elfang.com>
2025-01-25 22:18:21 -0500

    pix: Make lib extensible

--- lib/default.nix
+++ lib/default.nix
@@ -1,10 +1,10 @@
 { nixpkgs }:
 
 let
-  self = with nixpkgs.lib; foldl' (acc: x: acc // (callPackageWith { inherit nixpkgs self; } x {})) {} [
+  lib = self: with nixpkgs.lib; foldl' (acc: x: acc // (callPackageWith { inherit nixpkgs self; } x {})) {} [
     ./modules.nix
     ./prelude.nix
     ./trivial.nix
   ];
 
-in self
+in nixpkgs.lib.makeExtensible lib
