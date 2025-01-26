659f28e164a5dc4e06ca26b1088ad70519b53455
Fang Deng <fang@elfang.com>
2025-01-26 18:59:15 -0500

    pix: Add autoCall function to lib
    Also remove the use of `with' as much as possible to avoid confusions.

--- lib/filesystem.nix
+++ lib/filesystem.nix
@@ -1,19 +1,42 @@
 { self, nixpkgs }:
 
 let
-  lib = nixpkgs.lib;
+  inherit (nixpkgs.lib)
+    isFunction
+    functionArgs
+    intersectAttrs
+    attrNames
+    toString
+    readDir
+    baseNameOf
+    match
+    hasAttr
+    removeSuffix;
 
 in with self; {
+  /* Simplified version of `nixpkgs.lib.callPackageWith'.
+     This function doesn't add override attribute to the result.
+
+     Type:
+       autoCall :: AttrSet -> (AttrSet -> a) -> AttrSet -> a
+  */
+  autoCall = autoArgs: fn: args:
+    let
+      f = isFunction fn then fn else import fn;
+      passedArgs = intersectAttrs (functionArgs f) autoArgs // args;
+    in
+      f passedArgs;
+
   /* A generic function that filters all the files/directories under the given
      directory.  Return a list of names prepended with the given directory.
 
      Type:
        listDir :: (String -> String -> Bool) -> Path -> [String]
   */
-  listDir = pred: dir: with lib; builtins.attrNames
+  listDir = pred: dir: attrNames
     (filterAttrs pred (mapAttrs'
-      (name: type: nameValuePair (builtins.toString (dir + "/${name}")) type)
-      (builtins.readDir dir)));
+      (name: type: nameValuePair (toString (dir + "/${name}")) type)
+      (readDir dir)));
 
   /* Predications used for `listDir'. */
   notPred = pred: name: type: ! pred name type;
@@ -23,15 +46,15 @@ in with self; {
   isDirectoryType = name: type: type == "directory";
   isRegularType = name: type: type == "regular";
   isSymbolicType = name: type: type == "symlink";
-  isDefaultNix = name: type: (builtins.baseNameOf name) == "default.nix";
-  isNixFile = andPred isRegularType (name: type: builtins.match ".+\\.nix$" name != null);
-  isDisabled = name: type: builtins.match "^DISABLED_.*" name != null;
-  hasDefaultNix = andPred isDirectoryType (name: type: with builtins; hasAttr "default.nix"  (readDir name));
+  isDefaultNix = name: type: (baseNameOf name) == "default.nix";
+  isNixFile = andPred isRegularType (name: type: match ".+\\.nix$" name != null);
+  isDisabled = name: type: match "^DISABLED_.*" name != null;
+  hasDefaultNix = andPred isDirectoryType (name: type: hasAttr "default.nix"  (readDir name));
 
   /* Return the basename without .nix extension
 
      Type:
        baseNameNoNixExt :: String -> String
   */
-  baseNameNoNixExt = name: with lib; removeSuffix ".nix" (baseNameOf name);
+  baseNameNoNixExt = name: removeSuffix ".nix" (baseNameOf name);
 }
