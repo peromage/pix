ff3ca3d2162b00d30b062b5a5962ec4a6d36f672
Fang Deng <fang@elfang.com>
2025-02-03 16:49:30 -0500

    pix: Add packages from pot

--- packages/default.nix
+++ packages/default.nix
@@ -6,6 +6,11 @@ let
   pkgsCommon = {
     build-essential = ./common/build-essential.nix;
     home-manager = ./common/home-manager.nix;
+    pot-utils = ../dotfiles/pot-utils/pkgs/pot-utils.nix;
+    emacs = ../dotfiles/emacs/pkgs/emacs.nix;
+    spelling = ../dotfiles/emacs/pkgs/spelling.nix;
+    nodejs = ../dotfiles/nodejs/pkgs/nodejs.nix;
+    python = ../dotfiles/python/pkgs/python.nix;
   };
 
   pkgsPlatformSpecialized = {
