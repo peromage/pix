2ab255e4c522a8ffd5fb8d150d4e1d933f8fd251
Fang Deng <fang@elfang.com>
2025-02-03 20:23:43 -0500

    pot: Simplify stow.sh

--- dotfiles/stow.sh
+++ dotfiles/stow.sh
@@ -4,63 +4,38 @@ set -e
 
 function usage {
     >&2 cat <<EOF
+A simple wrapper of GNU Stow tailored for POT.
+
 Usage:
-  $0 ACTION [OPTIONS] pkg1 [pkg2 [...]]
+  $0 ACTION package [package [...]]
 
 Actions:
   -S    Stow packages
   -D    Delete packages
   -R    Restow packages
 
-Options:
-  -f    Enable folding (Default: no folding)
-  -h    Show this message
-  -v    Verbose output
+Note: Each package requires a sub-directory "home-files" which contains the dotfiles.
 EOF
 }
 
-## Need to append action and package name
-BASECMD="stow --dir=${BASH_SOURCE[0]%/*} --target=$HOME"
-
-action=
-opts="folding=0 verbose=0"
-package_list=()
-
 function run_stow {
-    local action="$1" && shift
-    local opts="$1" && shift
-    local args=
-    ! [[ "$opts" == *folding=1* ]] && args="--no-folding $args"
-    [[ "$opts" == *verbose=1* ]] && args="-v $args"
-
-    for p in "$@"; do
-        local cmd="${BASECMD} ${args} ${action} ${p}"
+    local action=$1 && shift
+    for pkg in "$@"; do
+        cmd="stow --dir=$pkg --target=$HOME --no-folding -v $action home-files"
         echo "> RUN: $cmd"
         eval "$cmd"
     done
 }
 
-while [[ "$OPTIND" -le $# ]]; do
-    if getopts "SDRfhv" o; then
-        case "$o" in
-            (S) action="-S" ;;
-            (D) action="-D" ;;
-            (R) action="-R" ;;
-            (f) opts="folding=1 $opts" ;;
-            (v) opts="verbose=1 $opts" ;;
-            (h) ;&
-            (*) usage; exit 1 ;;
-        esac
-    else
-        eval "package_list+=("${!OPTIND}")" # Indirect reference
-        let OPTIND++
-    fi
-done
-
-
-if [[ -z $action ]] || [[ ${#package_list} -eq 0 ]]; then
+if [[ $# -lt 2 ]]; then
     usage
     exit 1
 fi
 
-run_stow $action "$opts" "${package_list[@]}"
+case $1 in
+    (-S) ;&
+    (-D) ;&
+    (-R) run_stow "$@" ;;
+    (-h|--help) ;&
+    (*) usage
+esac
