61c90f74f980e0864c6ff17158d350c7b363e58a
Fang Deng <fang@elfang.com>
2025-02-23 11:45:16 -0500

    pix: Add support for XBox controller

--- modules/hardware/peripherals.nix
+++ modules/hardware/peripherals.nix
@@ -3,26 +3,36 @@
 let
   cfg = config.pix.hardware.peripherals;
 
+  mkDeviceConfig = device: config: lib.mkIf (lib.elem device cfg.devices) config;
+
 in {
   options.pix.hardware.peripherals = {
     enable = lib.mkEnableOption "peripheral management";
     devices = lib.mkOption {
-      type = with lib.types; listOf (enum [ "printer" "zsa-keyboard" ]);
+      type = with lib.types; listOf (enum [
+        "printer"
+        "zsa-keyboard"
+        "xbox-controller"
+      ]);
       default = [];
       description = "A list of devices to support.";
     };
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf (cfg.enable && lib.elem "printer" cfg.devices) {
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    (mkDeviceConfig "printer" {
       services.printing.enable = true;
     })
 
-    (lib.mkIf (cfg.enable && lib.elem "zsa-keyboard" cfg.devices) {
+    (mkDeviceConfig "zsa-keyboard" {
       hardware.keyboard.zsa.enable = true;
       environment.systemPackages = with pkgs; [
         keymapp
       ];
     })
-  ];
+
+    (mkDeviceConfig "xbox-controller" {
+      hardware.xpadneo.enable = true;
+    })
+  ]);
 }
