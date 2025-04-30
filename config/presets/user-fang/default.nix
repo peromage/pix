361b7ff0f6df6d6a549d8dfbba469b60cc967e21
Fang Deng <fang@elfang.com>
2025-04-30 12:35:25 -0400

    pix: Add pre-configured users to kvm group
    Possibly reduce the Android emulator's CPU usage?
    
    Source: https://nixos.wiki/wiki/Android#hardware_acceleration

--- config/presets/user-fang/default.nix
+++ config/presets/user-fang/default.nix
@@ -4,7 +4,7 @@
   config.pix.users.fang = {
     description = "Fang The Handsome";
     id = 1001;
-    groups = [ "wheel" "users" "audio" "video" "cdrom" "networkmanager" ];
+    groups = [ "wheel" "users" "audio" "video" "cdrom" "networkmanager" "kvm" ];
     enableNixManagement = true;
     password = lib.mkDefault "P@55w0rd";
   };
