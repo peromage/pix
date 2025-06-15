d3811daff4820022d8ec0748dcdca2cd1f855e0c
Fang Deng <fang@elfang.com>
2025-06-14 22:54:37 -0400

    pot@emacs: Expose spelling package

--- dotfiles/emacs/default.nix
+++ dotfiles/emacs/default.nix
@@ -1,21 +1,30 @@
 { config, pkgs, lib, ... }:
 
 let
-  cfg = config.pix.dotfiles.emacs;
+  emacsCfg = config.pix.dotfiles.emacs;
+  spellingCfg = config.pix.dotfiles.spelling;
   src = ./home-files/.emacs.d;
   myEmacs = pkgs.callPackage ./pkgs/emacs.nix {};
+  mySpelling = pkgs.callPackage ./pkgs/spelling.nix {};
 
 in {
-  options.pix.dotfiles.emacs = {
-    enable = lib.mkEnableOption "Pot Emacs";
+  options.pix.dotfiles = {
+    emacs.enable = lib.mkEnableOption "Pot Emacs";
+    spelling.enable = lib.mkEnableOption "Pot Spelling";
   };
 
-  config = lib.mkIf cfg.enable {
-    home.packages = [ myEmacs ];
+  config = lib.mkMerge [
+    (lib.mkIf emacsCfg.enable {
+      home.packages = [ myEmacs ];
 
-    home.file.".emacs.d" = {
-      source = src;
-      recursive = true;
-    };
-  };
+      home.file.".emacs.d" = {
+        source = src;
+        recursive = true;
+      };
+    })
+
+    (lib.mkIf spellingCfg.enable {
+      home.packages = [ mySpelling ];
+    })
+  ];
 }
