0a9523d5ad2be342bc15ab8912fc8a54df1f765a
Fang Deng <fang@elfang.com>
2025-10-02 16:53:43 -0400

    emacs: Remove ad-hoc mermaid installation
    It runs into a dependency issue with the latest update so prefer to use global installation.

--- dotfiles/emacs/home-files/.emacs.d/pew/lisp/elpa-langs.el
+++ dotfiles/emacs/home-files/.emacs.d/pew/lisp/elpa-langs.el
@@ -86,19 +86,7 @@
 
 (use-package mermaid-mode
   :ensure t
-  :mode ("\\.mmd\\'" . mermaid-mode)
-  :custom
-  (mermaid-mmdc-location (expand-file-name ".cache/mermaid/node_modules/.bin/mmdc" user-emacs-directory))
-
-  :preface
-  (defun pew::mermaid-mode::install-cli ()
-    "Install Mermaid CLI tool in user Emacs folder."
-    (interactive)
-    (let ((default-directory (substring mermaid-mmdc-location 0 (string-match-p "node_modules" mermaid-mmdc-location))))
-      (message "Installing mermaid-cli...")
-      (mkdir default-directory t)
-      (call-process "npm" nil nil nil "install" "@mermaid-js/mermaid-cli")
-      (message "Installing mermaid-cli... done"))))
+  :mode ("\\.mmd\\'" . mermaid-mode))
 
 ;; Mermaid `org-mode' support
 (use-package ob-mermaid
