5056b3075c7b315ab5ea36ecff7a300688815302
Fang Deng <fang@elfang.com>
2025-12-26 13:16:20 -0500

    pot: Copy darwin scripts separately

--- dotfiles/pot-utils/packages/pot-utils.nix
+++ dotfiles/pot-utils/packages/pot-utils.nix
@@ -1,14 +1,15 @@
-{ pkgs }:
+{ stdenvNoCC, lib }:
 
-pkgs.stdenvNoCC.mkDerivation {
+stdenvNoCC.mkDerivation {
   pname = "pot-utils";
   version = "0.0.1";
   src = ../home-files;
-  nativeBuildInputs = with pkgs; [];
   dontPatchShebangs = true;
 
   installPhase = ''
     mkdir -p $out/bin
     install --mode=555 $src/bin/* $out/bin/
-  '';
+  '' + (lib.optionalString stdenvNoCC.isDarwin ''
+    install --mode=555 $src/bin-darwin/* $out/bin/
+  '');
 }
