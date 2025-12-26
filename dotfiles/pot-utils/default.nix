3c9076a734926319ff25b8a65b917ba82fda2cc3
Fang Deng <fang@elfang.com>
2025-12-26 13:03:55 -0500

    pot: Refactor darwin options

--- dotfiles/pot-utils/default.nix
+++ dotfiles/pot-utils/default.nix
@@ -2,29 +2,23 @@
 
 let
   cfg = config.pix.dotfiles.pot-utils;
+  mkIfDarwin = cond: lib.mkIf (pkgs.stdenv.isDarwin && cond);
 
 in {
   options.pix.dotfiles.pot-utils = {
     enable = lib.mkEnableOption "Pot Utils";
-    fixMacOSApps = lib.mkEnableOption "Fix App shortcuts on MacOS";
+
+    # Options have no effect outside of MacOS
+    darwin = {
+      fixHomeManagerApps = lib.mkEnableOption "Fix Home Manager App shortcuts.";
+    };
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf cfg.enable {
-      home.packages = [ pkgs.pixPkgs.pot-utils ];
-    })
+  config = lib.mkIf cfg.enable {
+    home.packages = [ pkgs.pixPkgs.pot-utils ];
 
-    (lib.mkIf cfg.fixMacOSApps {
-      home.activation.fixMacOSApps = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        run ${pkgs.pixPkgs.pot-utils}/bin/fix-macos-hm-apps.sh
-      '';
-
-      assertions = [
-        {
-          assertion = with builtins; (match ".+-darwin" pkgs.stdenv.hostPlatform.system) != null;
-          message = "pix.pot-utils.fixMacOSApps must be used on MacOS!";
-        }
-      ];
-    })
-  ];
+    home.activation.fixMacOSApps = mkIfDarwin cfg.darwin.fixHomeManagerApps (lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      run ${pkgs.pixPkgs.pot-utils}/bin/macos-fix-homemanager-apps.sh
+    '');
+  };
 }
