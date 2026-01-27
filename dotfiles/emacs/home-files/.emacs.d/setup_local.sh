f7327d2df1c7e410f92eee6fd78f19bbdb5d5076
Fang Deng <fang@elfang.com>
2026-01-27 13:50:18 -0500

    emacs: Local setup scripts now can choose either required or optional

--- dotfiles/emacs/home-files/.emacs.d/setup_local.sh
+++ dotfiles/emacs/home-files/.emacs.d/setup_local.sh
@@ -2,10 +2,29 @@
 
 set -e
 
-if [[ $# -lt 1 ]]; then
-    echo "TARGET directory required"
-    exit 1
-fi
+function help_and_exit {
+    cat <<EOF
+Usage:
+  $(basename $0) [-r | -o] DIR
+
+Options:
+  -r    Set up only required files
+  -o    Set up only optional files
+
+By default, if neither '-r' nor '-o' is specified, both required and optional
+files are copied.
+EOF
+}
+
+while getopts "ro" opt "$@"; do
+    case "$opt" in
+        (r) SETUP_REQUIRED=1 ;;
+        (o) SETUP_OPTIONAL=1 ;;
+        (*) help_and_exit ;;
+    esac
+done
+
+shift $((OPTIND - 1))
 
 TARGET_DIR="$(realpath "$1")"
 SOURCE_DIR="$(realpath "${BASH_SOURCE[0]%/*}")"
@@ -22,15 +41,27 @@ OPTIONAL=(
     tree-sitter
 )
 
-for i in "${REQUIRED[@]}"; do
-    ln -sf "$SOURCE_DIR/$i" "$TARGET_DIR/"
-done
-
-for i in "${OPTIONAL[@]}"; do
-    if [[ -e "$SOURCE_DIR/$i" ]]; then
+function setup_required {
+    for i in "${REQUIRED[@]}"; do
         ln -sf "$SOURCE_DIR/$i" "$TARGET_DIR/"
-    fi
-done
+    done
+}
+
+function setup_optional {
+    for i in "${OPTIONAL[@]}"; do
+        if [[ -e "$SOURCE_DIR/$i" ]]; then
+            ln -sf "$SOURCE_DIR/$i" "$TARGET_DIR/"
+        fi
+    done
+}
+
+[[ "$SETUP_REQUIRED" -eq 1 ]] && setup_required
+[[ "$SETUP_OPTIONAL" -eq 1 ]] && setup_optional
+
+if [[ -z "$SETUP_REQUIRED" ]] && [[ -z "$SETUP_OPTIONAL" ]]; then
+    setup_required
+    setup_optional
+fi
 
 cat <<EOF > "$TARGET_DIR/run.sh"
 emacs --init-directory='$TARGET_DIR' --debug-init "\$@"
