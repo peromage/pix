fba8c3fa20855345c77d96fb005e0dc9898f0ee7
Fang Deng <fang@elfang.com>
2026-01-27 12:58:34 -0500

    emacs: Update local setup script

--- dotfiles/emacs/home-files/.emacs.d/setup_local.sh
+++ dotfiles/emacs/home-files/.emacs.d/setup_local.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ $# -lt 1 ]; then
+if [[ $# -lt 1 ]]; then
     echo "TARGET directory required"
     exit 1
 fi
@@ -10,12 +10,27 @@ fi
 TARGET_DIR="$(realpath "$1")"
 SOURCE_DIR="$(realpath "${BASH_SOURCE[0]%/*}")"
 
-ln -sf \
-   "$SOURCE_DIR/init.el" \
-   "$SOURCE_DIR/init-mini.el" \
-   "$SOURCE_DIR/early-init.el" \
-   "$SOURCE_DIR/pew" \
-   "$TARGET_DIR"
+REQUIRED=(
+    init.el
+    init-mini.el
+    early-init.el
+    pew
+)
+
+OPTIONAL=(
+    straight
+    tree-sitter
+)
+
+for i in "${REQUIRED[@]}"; do
+    ln -sf "$SOURCE_DIR/$i" "$TARGET_DIR/"
+done
+
+for i in "${OPTIONAL[@]}"; do
+    if [[ -e "$SOURCE_DIR/$i" ]]; then
+        ln -sf "$SOURCE_DIR/$i" "$TARGET_DIR/"
+    fi
+done
 
 cat <<EOF > "$TARGET_DIR/run.sh"
 emacs --init-directory='$TARGET_DIR' --debug-init "\$@"
