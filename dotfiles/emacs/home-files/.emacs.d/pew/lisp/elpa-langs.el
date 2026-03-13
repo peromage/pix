444fef4551af152c9a794ccfa66b90b1e831856c
Fang Deng <fang@elfang.com>
2026-03-13 14:52:16 -0400

    Revert "emacs: Remove ad-hoc mermaid installation"
    This reverts commit 0a9523d5ad2be342bc15ab8912fc8a54df1f765a.

--- dotfiles/emacs/home-files/.emacs.d/pew/lisp/elpa-langs.el
+++ dotfiles/emacs/home-files/.emacs.d/pew/lisp/elpa-langs.el
@@ -265,7 +265,19 @@ NOTE: For 'query' matchers, the sexp 'query' won't work unless they are compiled
 
 
 (use-package mermaid-mode
-  :mode ("\\.mmd\\'" . mermaid-mode))
+  :mode ("\\.mmd\\'" . mermaid-mode)
+  :custom
+  (mermaid-mmdc-location (expand-file-name ".cache/mermaid/node_modules/.bin/mmdc" user-emacs-directory))
+
+  :preface
+  (defun pew::mermaid-mode::install-cli ()
+    "Install Mermaid CLI tool in user Emacs folder."
+    (interactive)
+    (let ((default-directory (substring mermaid-mmdc-location 0 (string-match-p "node_modules" mermaid-mmdc-location))))
+      (message "Installing mermaid-cli...")
+      (mkdir default-directory t)
+      (call-process "npm" nil nil nil "install" "@mermaid-js/mermaid-cli")
+      (message "Installing mermaid-cli... done"))))
 
 
 ;; Mermaid `org-mode' support
