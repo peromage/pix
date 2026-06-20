fc66d2bb75a259764cd4d9137e0294a6cd1b7159
Fang Deng <fang@elfang.com>
2026-06-20 18:28:04 -0400

    nix: Upgrade to release-26.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "PIX - Peromage's nIX configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
-    home-manager = { url = "github:nix-community/home-manager/release-25.11"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
+    home-manager = { url = "github:nix-community/home-manager/release-26.05"; inputs.nixpkgs.follows = "nixpkgs"; };
 
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
     lanzaboote = { url = "github:nix-community/lanzaboote/master"; inputs.nixpkgs.follows = "nixpkgs"; };
