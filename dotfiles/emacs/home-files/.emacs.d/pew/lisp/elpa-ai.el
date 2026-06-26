9d29a28cbe54c7d2416cda61edecb8e2572c374f
Fang Deng <fang@elfang.com>
2026-06-26 00:04:50 -0400

    emacs: Update claude-code-ide config

--- dotfiles/emacs/home-files/.emacs.d/pew/lisp/elpa-ai.el
+++ dotfiles/emacs/home-files/.emacs.d/pew/lisp/elpa-ai.el
@@ -28,9 +28,25 @@
 
 (use-package claude-code-ide
   :straight (:type git :host github :repo "manzaltu/claude-code-ide.el")
+
   :bind
   ( :map pew-M-l-map
-    ("d" . claude-code-ide-menu)))
+    ("d" . claude-code-ide-menu))
+
+  :custom
+  (claude-code-ide-window-side 'right)
+  (claude-code-ide-window-width 100)
+  (claude-code-ide-use-side-window nil)  ; Use regular window
+  (claude-code-ide-diagnostics-backend 'flymake)
+  (claude-code-ide-enable-execute-code t)
+  (claude-code-ide-cli-extra-flags "--model opus")
+  ;; Vterm related
+  (claude-code-ide-terminal-backend 'vterm)
+  (claude-code-ide-vterm-anti-flicker t)
+  (claude-code-ide-vterm-render-delay 0.01)
+
+  :config
+  (claude-code-ide-emacs-tools-setup))
 
 (provide 'elpa-ai)
 ;;; elpa-ai.el ends here
