c4271dbdfe0515c789a106b3ed74a73c96fbc444
Fang Deng <fang@elfang.com>
2026-06-26 13:18:47 -0400

    emacs: Update claude-code config to use ghostel

--- dotfiles/emacs/home-files/.emacs.d/pew/lisp/elpa-ai.el
+++ dotfiles/emacs/home-files/.emacs.d/pew/lisp/elpa-ai.el
@@ -34,14 +34,14 @@
     ("d" . claude-code-ide-menu))
 
   :custom
+  (claude-code-ide-use-side-window t)
   (claude-code-ide-window-side 'right)
   (claude-code-ide-window-width 100)
-  (claude-code-ide-use-side-window nil)  ; Use regular window
   (claude-code-ide-diagnostics-backend 'flymake)
   (claude-code-ide-enable-execute-code t)
   (claude-code-ide-cli-extra-flags "--model opus")
   ;; Vterm related
-  (claude-code-ide-terminal-backend 'vterm)
+  (claude-code-ide-terminal-backend 'ghostel)
   (claude-code-ide-vterm-anti-flicker t)
   (claude-code-ide-vterm-render-delay 0.01)
 
