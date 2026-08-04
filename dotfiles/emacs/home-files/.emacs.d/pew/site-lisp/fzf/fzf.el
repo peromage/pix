00b7bac35997890782f2990273d3df388037dfbe
Fang Deng <fang@elfang.com>
2026-08-04 14:07:46 -0400

    emacs: Make fzf plugin extensible

--- dotfiles/emacs/home-files/.emacs.d/pew/site-lisp/fzf/fzf.el
+++ dotfiles/emacs/home-files/.emacs.d/pew/site-lisp/fzf/fzf.el
@@ -2,14 +2,32 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun fzf-find-file (&optional directory)
+(defvar fzf-terms
+  '((vterm fzf--vterm-create-buffer fzf--vterm-execute))
+  "Alist to specify terminal backend handlers.
+Each element is of the form (TERM . (TERM-CREATION-FUNC TERM-EXECUTION-FUNC))")
+
+(defvar fzf-default-term 'vterm
+  "Default terminal backend.")
+
+(defun fzf--vterm-create-buffer (name)
+  (let ((default-directory directory))
+    (vterm name)))
+
+(defun fzf--vterm-execute (command)
+  (vterm-send-string command)
+  (vterm-send-return))
+
+(defun fzf-find-file (directory &optional term)
   "Use fzf to find file in the given DIRECTORY.
 
 Dependencies:
   - fzf (must in PATH)
   - vterm (Emacs plugin)"
   (interactive "DSearch in: ")
-  (let* ((cache (make-temp-file "fzf-"))
+  (let* ((name "fzf-find-file")
+         (cache (make-temp-file (format "%s-" name)))
+         (command (format "exec fzf >%s" cache))
          (callback (lambda ()
                      (with-temp-buffer
                        (insert-file-contents-literally cache)
@@ -17,13 +35,11 @@ Dependencies:
                        (let ((result (string-trim (buffer-string))))
                          (when result
                            (find-file (expand-file-name result directory)))))))
-         (vterm-buffer (save-excursion
-                         (let ((default-directory directory))
-                           (vterm "fzf-find-file")))))
-    (with-current-buffer vterm-buffer
-      (add-hook 'kill-buffer-hook callback nil t)
-      (vterm-send-string (format "exec fzf >%s" cache))
-      (vterm-send-return))))
+         (handlers (cdr (assq (or term fzf-default-term) fzf-terms)))
+         (buffer (save-excursion (funcall (car handlers) name))))
+    (with-current-buffer buffer
+      (add-hook 'kill-buffer-hook callback nil 'local)
+      (funcall (cadr handlers) command))))
 
 (provide 'fzf)
 ;;; fzf.el ends here
