e4562be7562171bb5f1165b7c02ff52a9a06ab5f
Fang Deng <fang@elfang.com>
2026-08-04 14:32:50 -0400

    emacs: Add ghostel support to fzf plugin

--- dotfiles/emacs/home-files/.emacs.d/pew/site-lisp/fzf/fzf.el
+++ dotfiles/emacs/home-files/.emacs.d/pew/site-lisp/fzf/fzf.el
@@ -2,21 +2,31 @@
 ;;; Commentary:
 ;;; Code:
 
-(defvar fzf-terms
-  '((vterm fzf--vterm-create-buffer fzf--vterm-execute))
-  "Alist to specify terminal backend handlers.
-Each element is of the form (TERM . (TERM-CREATION-FUNC TERM-EXECUTION-FUNC))")
+(require 'ghostel)
 
-(defvar fzf-default-term 'vterm
+(defvar fzf-default-term 'ghostel
   "Default terminal backend.")
 
+(defvar fzf-terms
+  '((vterm fzf--vterm-create-buffer fzf--vterm-execute)
+    (ghostel fzf--ghostel-create-buffer fzf--ghostel-execute))
+  "Alist to specify terminal backend handlers.
+Each element is of the form (TERM . (CREATION-FUNC EXECUTION-FUNC))")
+
 (defun fzf--vterm-create-buffer (name)
-  (let ((default-directory directory))
-    (vterm name)))
+  (vterm name))
 
 (defun fzf--vterm-execute (command)
   (vterm-send-string command)
   (vterm-send-return))
+
+(defun fzf--ghostel-create-buffer (name)
+  (let ((ghostel-buffer-name name))
+    (ghostel)))
+
+(defun fzf--ghostel-execute (command)
+  (ghostel-send-string command)
+  (ghostel-send-key "return"))
 
 (defun fzf-find-file (directory &optional term)
   "Use fzf to find file in the given DIRECTORY.
@@ -36,6 +46,7 @@ Dependencies:
                          (when result
                            (find-file (expand-file-name result directory)))))))
          (handlers (cdr (assq (or term fzf-default-term) fzf-terms)))
+         (default-directory directory)
          (buffer (save-excursion (funcall (car handlers) name))))
     (with-current-buffer buffer
       (add-hook 'kill-buffer-hook callback nil 'local)
